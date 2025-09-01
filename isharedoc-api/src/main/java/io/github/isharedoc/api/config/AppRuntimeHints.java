63913e06ae53753703338a2cc1e225700bcf499f
Sergey Ch. <the.sergiusac@gmail.com>
2025-09-02 00:37:14 +0500

    fix event processing

--- isharedoc-api/src/main/java/io/github/isharedoc/api/config/AppRuntimeHints.java
+++ isharedoc-api/src/main/java/io/github/isharedoc/api/config/AppRuntimeHints.java
@@ -1,9 +1,6 @@
 package io.github.isharedoc.api.config;
 
-import io.github.isharedoc.api.event.DeleteFileMetadataEvent;
-import io.github.isharedoc.api.event.DynamoStreamEventRecord;
-import io.github.isharedoc.api.event.EventSourceRecord;
-import io.github.isharedoc.api.event.SqsEventRecord;
+import io.github.isharedoc.api.event.*;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 
@@ -11,6 +8,7 @@ public class AppRuntimeHints implements RuntimeHintsRegistrar {
 
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        hints.serialization().registerType(EventRequest.class);
         hints.serialization().registerType(EventSourceRecord.class);
         hints.serialization().registerType(SqsEventRecord.class);
         hints.serialization().registerType(DynamoStreamEventRecord.class);
--- isharedoc-api/src/main/java/io/github/isharedoc/api/event/EventRequest.java
+++ isharedoc-api/src/main/java/io/github/isharedoc/api/event/EventRequest.java
@@ -2,10 +2,11 @@ package io.github.isharedoc.api.event;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.Serializable;
 import java.util.List;
 
 public record EventRequest(
         @JsonProperty("Records")
         List<String> records
-) {
+) implements Serializable {
 }
