5bcf69d5f9398c157b9e20a4e41f7df607b06daf
Sergey Ch. <the.sergiusac@gmail.com>
2025-09-02 00:27:15 +0500

    update event processing

--- isharedoc-api/src/main/java/io/github/isharedoc/api/config/AppRuntimeHints.java
+++ isharedoc-api/src/main/java/io/github/isharedoc/api/config/AppRuntimeHints.java
@@ -1,6 +1,9 @@
 package io.github.isharedoc.api.config;
 
 import io.github.isharedoc.api.event.DeleteFileMetadataEvent;
+import io.github.isharedoc.api.event.DynamoStreamEventRecord;
+import io.github.isharedoc.api.event.EventSourceRecord;
+import io.github.isharedoc.api.event.SqsEventRecord;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 
@@ -8,6 +11,9 @@ public class AppRuntimeHints implements RuntimeHintsRegistrar {
 
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        hints.serialization().registerType(EventSourceRecord.class);
+        hints.serialization().registerType(SqsEventRecord.class);
+        hints.serialization().registerType(DynamoStreamEventRecord.class);
         hints.serialization().registerType(DeleteFileMetadataEvent.class);
     }
 
